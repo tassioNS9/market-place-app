275c3fd83ad0a230b1fa061dbf5d03eec4d48f60
Tassio Neves <tassions@hotmail.com>
2026-09-16 10:53:06 -0300

    fix: change  field avatarURL to optional

--- src/shared/interfaces/http/register.ts
+++ src/shared/interfaces/http/register.ts
@@ -3,7 +3,7 @@ import { UserInterface } from "../user";
 export interface RegisterHttpParams {
   name: string;
   email: string;
-  avatarUrl: string;
+  avatarUrl?: string;
   phone: string;
   password: string;
 }
