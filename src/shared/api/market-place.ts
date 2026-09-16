0d22d5f4970ad649211aa401db89fce5b6d79c56
Tassio Neves <tassions@hotmail.com>
2026-09-16 10:51:41 -0300

    feat: configure base URL for axios instance based on platform

--- src/shared/api/market-place.ts
+++ src/shared/api/market-place.ts
@@ -1,4 +1,14 @@
 import axios, { AxiosInstance } from "axios";
+import { Platform } from "react-native";
+
+const getBaseURL = () => {
+  return Platform.select({
+    ios: "http://localhost:3001",
+    android: "http://10.0.2.2:3001",
+  });
+};
+
+const baseURL = getBaseURL();
 
 export class MarketPlaceApiClient {
   private instance: AxiosInstance;
@@ -6,7 +16,7 @@ export class MarketPlaceApiClient {
 
   constructor() {
     this.instance = axios.create({
-      baseURL: "",
+      baseURL,
     });
   }
 
