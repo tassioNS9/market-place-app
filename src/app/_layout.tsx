0c50da29e22074d8cfef714c6f38e6aeb9f05a78
Tassio Neves <tassions@hotmail.com>
2026-09-16 09:11:53 -0300

    chore: config privates routes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout() {
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="login" />
         <Stack.Screen name="register" />
+        <Stack.Screen name="(private)" />
       </Stack>
     </QueryClientProvider>
   );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,11 @@
 import { Redirect } from "expo-router";
 
 export default function App() {
+  const userData = null;
+
+  if (userData) {
+    return <Redirect href="/(private)/home" />;
+  }
+
   return <Redirect href="/login" />;
 }
