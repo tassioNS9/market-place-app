9ea37acba8c16029cdd989f75d29b3d1015a2a39
Tassio Neves <tassions@hotmail.com>
2026-09-16 09:05:56 -0300

    chore: simplify app entry point with redirect to login

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,5 @@
-import { router } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Redirect } from "expo-router";
 
 export default function App() {
-  return (
-    <View>
-      <Text className="text-lg font-bold">Olá mundo!</Text>
-      <TouchableOpacity onPress={() => router.push("login")}>
-        <Text className="text-lg font-bold text-red-500">Login</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return <Redirect href="/login" />;
 }
