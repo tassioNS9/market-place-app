91d577d3ec068256fe5fa19b1d9ecc4dcb3c4761
Tassio Neves <tassions@hotmail.com>
2026-09-17 16:21:29 -0300

    feat: add AppInput component in RegisterView

--- src/viewModels/Register/Register.view.tsx
+++ src/viewModels/Register/Register.view.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { useRegisterViewModel } from "./useRegister.viewModel";
+import { AppInput } from "@/shared/components/AppInput";
 
 export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
   onSubmit,
 }) => {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text>Register</Text>
+      <AppInput />
       <TouchableOpacity onPress={onSubmit}>
         <Text>Registrar</Text>
       </TouchableOpacity>
