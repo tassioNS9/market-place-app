43e702b8711432a7cabc31f0ab91cae522033064
Tassio Neves <tassions@hotmail.com>
2026-09-22 10:54:32 -0300

    feat: add AuthFormHeader component

---
+++ src/shared/components/AuthFormHeader/index.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { Image, Text, View } from "react-native";
+
+interface AuthFormHeaderProps {
+  title: string;
+  subTitle: string;
+}
+
+export const AuthFormHeader: FC<AuthFormHeaderProps> = ({
+  title,
+  subTitle,
+}) => {
+  return (
+    <View className="items-center mb-8">
+      <Image
+        source={require("@/assets/images/Logo.png")}
+        resizeMode="contain"
+        className="w-[80px] h-[60px] mb-8"
+      />
+
+      <Text className="text-3xl font-bold mb-3 text-gray-500">{title}</Text>
+      <Text className="text-base text-gray-300">{subTitle}</Text>
+    </View>
+  );
+};
