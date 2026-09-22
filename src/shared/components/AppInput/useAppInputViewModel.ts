18cb3cf2630918c7e92410f27ba24d1d83db2b65
Tassio Neves <tassions@hotmail.com>
2026-09-22 13:08:36 -0300

    feat: enhance RegisterView with additional input fields and improved layout

--- src/shared/components/AppInput/useAppInputViewModel.ts
+++ src/shared/components/AppInput/useAppInputViewModel.ts
@@ -52,7 +52,6 @@ export const useAppInputViewModel = ({
   const getIconColor = () => {
     if (isError) return colors.danger;
     if (isFocused) return colors["purple-base"];
-
     if (value) return colors["purple-base"];
     return colors.gray[200];
   };
--- src/viewModels/Register/Register.view.tsx
+++ src/viewModels/Register/Register.view.tsx
@@ -1,24 +1,74 @@
+import { router } from "expo-router";
 import { FC } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
-import { useRegisterViewModel } from "./useRegister.viewModel";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
 import { AppInputController } from "@/shared/components/AppInputController";
+import { AuthFormHeader } from "@/shared/components/AuthFormHeader";
+import { useRegisterViewModel } from "./useRegister.viewModel";
+import { KeyboardContainer } from "@/shared/components/KeyboardContainer";
 
 export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
   onSubmit,
   control,
 }) => {
   return (
-    <View className="flex-1 justify-center">
-      <AppInputController
-        label="E-mail"
-        leftIcon="person"
-        name="email"
-        control={control}
-        placeholder="E-mail"
-      />
-      <TouchableOpacity onPress={onSubmit}>
-        <Text>Registrar</Text>
-      </TouchableOpacity>
-    </View>
+    <KeyboardContainer>
+      <ScrollView className="flex-1 px-[40px]">
+        <AuthFormHeader
+          title="Crie sua conta"
+          subTitle="Informe seus dados pessoais e de acesso"
+        />
+        <AppInputController
+          leftIcon="person-outline"
+          label="NOME"
+          control={control}
+          name="name"
+          placeholder="Seu nome completo"
+        />
+
+        <AppInputController
+          leftIcon="call-outline"
+          label="TELEFONE"
+          control={control}
+          name="phone"
+          placeholder="(00) 00000-0000"
+        />
+
+        <Text className="text-base mt-6 font-bold text-gray-500">Acesso</Text>
+
+        <AppInputController
+          leftIcon="mail-outline"
+          label="E-MAIL"
+          control={control}
+          name="email"
+          placeholder="mail@example.com.br"
+        />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="SENHA"
+          control={control}
+          name="password"
+          placeholder="Sua senha"
+          secureTextEntry
+        />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="CONFIRMAR SENHA"
+          control={control}
+          name="confirmPassword"
+          placeholder="Confirme sua senha"
+          secureTextEntry
+        />
+
+        <TouchableOpacity onPress={onSubmit}>
+          <Text>Registrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push("/login")}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardContainer>
   );
 };
