233e356c96559a948c81f82b73c28983945863d1
Tassio Neves <tassions@hotmail.com>
2026-09-24 20:33:59 -0300

    feat: update useGallery hook to improve permission handling and state naming

--- src/shared/hooks/useGallery.ts
+++ src/shared/hooks/useGallery.ts
@@ -1,10 +1,11 @@
 import * as ImagePicker from "expo-image-picker";
 import { ImagePickerOptions } from "expo-image-picker";
 import { useCallback, useState } from "react";
+import { Alert, Linking } from "react-native";
 import { Toast } from "toastify-react-native";
 
 export const useGallery = (pickerOptions: ImagePickerOptions) => {
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
   const requestGalleryPermission = useCallback(async () => {
     try {
@@ -13,7 +14,22 @@ export const useGallery = (pickerOptions: ImagePickerOptions) => {
       const currentStatus = status === "granted";
 
       if (!currentStatus) {
-        Toast.error("Precisamos da permissão para acessar suas fotos", "top");
+        Alert.alert(
+          "Permissão negada!",
+          "Precisamos de permissão para acessar sua galeria de fotos",
+          [
+            {
+              text: "Cancelar",
+              style: "cancel",
+            },
+            {
+              text: "Abrir configurações",
+              onPress: () => {
+                Linking.openSettings();
+              },
+            },
+          ],
+        );
       }
 
       return currentStatus;
@@ -44,6 +60,5 @@ export const useGallery = (pickerOptions: ImagePickerOptions) => {
       setLoading(false);
     }
   }, []);
-
-  return { openGallery, loading };
+  return { openGallery, isLoading };
 };
