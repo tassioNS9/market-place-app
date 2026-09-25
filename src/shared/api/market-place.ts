a2b3d9f3771ad4dc67ab730af7f18fc0597ccf72
Tassio Neves <tassions@hotmail.com>
2026-09-25 13:15:53 -0300

    feat: enhance MarketPlaceApiClient to include authorization token in request headers

--- src/shared/api/market-place.ts
+++ src/shared/api/market-place.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 import { Platform } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const getBaseURL = () => {
   return Platform.select({
@@ -8,20 +9,45 @@ const getBaseURL = () => {
   });
 };
 
-const baseURL = getBaseURL();
+export const baseURL = getBaseURL();
 
 export class MarketPlaceApiClient {
   private instance: AxiosInstance;
-  private isRefreshing = false;
 
   constructor() {
     this.instance = axios.create({
       baseURL,
     });
+    this.setupInterceptors();
   }
 
   getInstance() {
     return this.instance;
+  }
+
+  private setupInterceptors() {
+    this.instance.interceptors.request.use(
+      async (config) => {
+        const userData = await AsyncStorage.getItem("marketplace-auth");
+        console.log(userData);
+        if (userData) {
+          const {
+            state: { token },
+          } = JSON.parse(userData);
+
+          console.log(token);
+
+          if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+          }
+        }
+
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      },
+    );
   }
 }
 
