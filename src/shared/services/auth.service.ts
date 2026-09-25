040dd18426614f66384bdfc47a98053294e6fda2
Tassio Neves <tassions@hotmail.com>
2026-09-25 13:16:11 -0300

    feat: add UploadAvatarResponse interface and implement uploadAvatar function in auth service

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,7 +1,8 @@
-import { marketPlaceApiClient } from "../api/market-place";
+import { marketPlaceApiClient, baseURL } from "../api/market-place";
 import { LoginHttpParams } from "../interfaces/http/login";
 import { AuthResponse } from "../interfaces/http/auth-response";
 import { RegisterHttpParams } from "../interfaces/http/register";
+import { UploadAvatarResponse } from "../interfaces/http/upload-avatar";
 
 export const register = async (userData: RegisterHttpParams) => {
   const { data } = await marketPlaceApiClient.post<AuthResponse>(
@@ -17,6 +18,25 @@ export const login = async (userData: LoginHttpParams) => {
     "/auth/login",
     userData,
   );
+
+  return data;
+};
+
+export const uploadAvatar = async (avatarUri: string) => {
+  const formData = new FormData();
+
+  formData.append("avatar", {
+    uri: avatarUri,
+    type: "image/jpeg",
+    name: "avatar.jpeg",
+  } as unknown as Blob);
+
+  const { data } = await marketPlaceApiClient.post<UploadAvatarResponse>(
+    "/user/avatar",
+    formData,
+  );
+
+  data.url = `${baseURL}${data.url}`;
 
   return data;
 };
