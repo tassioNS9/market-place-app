8dce81b114e8c4756c75ded2fd18db102807c271
Tassio Neves <tassions@hotmail.com>
2026-09-25 14:29:44 -0300

    feat: update useLoginMutation to set user session on successful login

--- src/shared/queries/auth/use-login.mutation.ts
+++ src/shared/queries/auth/use-login.mutation.ts
@@ -1,12 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
 import { LoginHttpParams } from "../../interfaces/http/login";
 import { login } from "../../services/auth.service";
+import { useUserStore } from "@/shared/store/user-store";
 
 export const useLoginMutation = () => {
+  const { setSession } = useUserStore();
   const mutation = useMutation({
     mutationFn: (userData: LoginHttpParams) => login(userData),
     onSuccess: (response) => {
-      console.log(response);
+      setSession(response);
     },
     onError: (error) => {
       console.log(error);
