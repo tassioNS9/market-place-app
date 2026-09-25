832d438b99bf81897fd4be1cc866199ce9315c71
Tassio Neves <tassions@hotmail.com>
2026-09-25 14:52:58 -0300

    feat: implement token refresh logic in MarketPlaceApiClient

--- src/shared/api/market-place.ts
+++ src/shared/api/market-place.ts
@@ -1,11 +1,11 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios, { AxiosInstance } from "axios";
 import { Platform } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const getBaseURL = () => {
   return Platform.select({
     ios: "http://localhost:3001",
-    android: "http://localhost:3001",
+    android: "http://10.0.2.2:3001",
   });
 };
 
@@ -13,11 +13,13 @@ export const baseURL = getBaseURL();
 
 export class MarketPlaceApiClient {
   private instance: AxiosInstance;
+  private isRefreshing = false;
 
   constructor() {
     this.instance = axios.create({
       baseURL,
     });
+
     this.setupInterceptors();
   }
 
@@ -35,8 +37,6 @@ export class MarketPlaceApiClient {
             state: { token },
           } = JSON.parse(userData);
 
-          console.log(token);
-
           if (token) {
             config.headers.Authorization = `Bearer ${token}`;
           }
@@ -46,6 +46,67 @@ export class MarketPlaceApiClient {
       },
       (error) => {
         return Promise.reject(error);
+      },
+    );
+
+    this.instance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (
+          error.response?.status === 401 &&
+          error.response?.data?.message === "Token expirado" &&
+          !this.isRefreshing
+        ) {
+          this.isRefreshing = true;
+
+          try {
+            const userData = await AsyncStorage.getItem("marketplace-auth");
+
+            if (!userData) {
+              throw new Error("Usuário não autenticado");
+            }
+
+            const {
+              state: { refreshToken },
+            } = JSON.parse(userData);
+
+            if (!refreshToken) {
+              throw new Error("Refresh token não encontrado");
+            }
+
+            const { data: response } = await axios.post("/auth/refresh", {
+              refreshToken,
+            });
+
+            const currentUserData = JSON.parse(userData);
+
+            currentUserData.state.token = response.token;
+            currentUserData.state.refreshToken = response.refreshToken;
+
+            await AsyncStorage.setItem(
+              "marketplace-auth",
+              JSON.stringify(currentUserData),
+            );
+
+            originalRequest.headers.Authorization = `Bearer ${response.token}`;
+
+            return axios(originalRequest);
+          } catch {
+            return Promise.reject(
+              new Error("Sessão expirada, faça o login novamente."),
+            );
+          } finally {
+            this.isRefreshing = false;
+          }
+        }
+
+        if (error.response && error.response.data) {
+          return Promise.reject(new Error(error.response.data.message));
+        } else {
+          return Promise.reject(new Error("Falha na requisição"));
+        }
       },
     );
   }
